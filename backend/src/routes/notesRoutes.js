e30ebb896a60b0c8fbca20686dd64896cc464b67
STEALTH-GOD <samyaklal111@gmail.com>
2025-11-19 18:12:28 +0545

    fixed secret

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -12,9 +12,4 @@ router.delete("/:id", deleteNote);
 
 export default router;
 
-// samyakrock11_db_user
-// eIkLDQVJUlBay9Lx
-// k7bKVe3zdiVPX7lf
 
-
-// mongodb+srv://samyakrock11_db_user:k7bKVe3zdiVPX7lf@cluster0.3ll8sro.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
